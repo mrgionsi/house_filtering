faaeaafb8b80b1236c019396f34927328f0e7e07
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-02-04 23:50:22 +0100

    fixed check on row

--- backend/utilities.py
+++ backend/utilities.py
@@ -11,7 +11,7 @@ from data_handler import JsonObject, addEntry
 
 #url=f"https://www.immobiliare.it/api-next/search-list/real-estates/?vrt=41.104579%2C14.31879%3B41.099017%2C14.272099%3B41.070163%2C14.250641%3B41.037542%2C14.272614%3B41.022391%2C14.288063%3B41.023298%2C14.324284%3B41.033399%2C14.34454%3B41.064339%2C14.362907%3B41.090996%2C14.348831%3B41.104579%2C14.31879&idContratto=1&idCategoria=1&__lang=it&pag={count_page}&paramsCount=6&path=%2Fsearch-list%2F"
 _DEBUG=False
-_DELAY=15
+_DELAY=1
 logging.basicConfig(
     encoding="utf-8",
     level=logging.INFO,
@@ -42,7 +42,7 @@ def fetch_data(page_number):
 
 
 def start_search_scraping():
-        count_page=1
+        count_page=16
         _DEBUG=False
         _DELAY=15
         if not _DEBUG:
@@ -71,14 +71,15 @@ def start_search_scraping():
                 if 'supervisor' in advertiser:
                 ### here check if label is privato or label exists. If not, it should be private anyway
                 ##    if advertiser['supervisor']['label'] == 'privato':
-                    if (
+                    if ((
                         'supervisor' in advertiser and 
                         isinstance(advertiser['supervisor'], dict) and
                         (
                             ##'label' in advertiser['supervisor'] or 
                             ('type' in advertiser['supervisor'] and advertiser['supervisor']['type'] == 'user')
                         )
-                    ):   
+                    ) and ('title' in res['seo'])):   
+                       
                         house = JsonObject(
                             id=res['realEstate']['id'],  # Directly assign value, no {}
                             title=res['seo']['title'],  # Directly assign value, no {}
@@ -88,7 +89,6 @@ def start_search_scraping():
                             img = res['realEstate']['properties'][0].get('photo', {}).get('urls', {}).get('medium', ""),
                             date_added=str(date.today()),
                             clicked=False)  # Correct date format
-                        
                         yield from addEntry(house)
 
                         logger.info(f"Titolo: {res['seo']['title']} - {res['realEstate']['price']['formattedValue']} ")
